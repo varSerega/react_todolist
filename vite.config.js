2461b159a800a536e09efddf28016e0221b46040
varSerega <bozhenko15rpus@gmail.com>
2026-02-07 14:07:40 +0300

    commit 7

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => {
     const isProd = mode === 'production'
 
     return {
-        base: isProd ? '/react-todolist/' : '/',
+        base: isProd ? '/react_todolist/' : '/',
         plugins: [react()],
         resolve: {
             alias: {
